fe2a28de116ba1ee744623c9fc5a1d160d97cd64
Yordan Petrov <dakatapetrov@gmail.com>
2024-06-10 11:46:40 +0300

    nvim: use 4 spaces as tabs; show cursor line

--- nvim/lua/opts.lua
+++ nvim/lua/opts.lua
@@ -38,3 +38,10 @@ vim.filetype.add({ extension = {
     psql = "sql",
   },
 })
+
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+
+vim.o.cursorline = true
